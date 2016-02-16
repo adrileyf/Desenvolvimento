bc836a504f68a9f91c1102013e2412adc89c5a64
adrileyf <adrileywork@gmail.com>
2016-02-16 04:15:32 -0300

    novo

---
+++ desenvolvimento/baixar.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="baixar.aspx.cs" Inherits="VhsAsp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/Site.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+      <h1>VHSDesenvolvimento LTDA.</h1> 
+      <h2> Desenvolvimento de Sistema para Integração Contábil, Versão para web.</h2>
+    
+    </div>
+        <asp:Label ID="Label1" runat="server" Text="Selecione o Arquivo Download"></asp:Label>
+        <br />
+        <asp:ListBox ID="ListBox1" runat="server" Height="162px" Width="354px"></asp:ListBox>
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click1" Text="Baixar" />
+        <br />
+        
+    </form>
+</body>
+</html>
---
+++ desenvolvimento/baixar.aspx.cs
@@ -0,0 +1,65 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VhsAsp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            //{ 
+            //  Dim objConexao As MySqlConnection = New MySqlConnection(ConfigurationManager.AppSettings("strConexao"))
+            //Dim command As MySqlCommand = New MySqlCommand(sql, objConexao)
+            
+            //}
+
+            string sqlQuery = "SELECT * FROM tipo_cfop  ";
+
+            //MySqlConnection objConexao = new MySqlConnection(ConfigurationManager.AppSettings["MYconn"]);
+            //MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            //objAdapter.SelectCommand = new MySqlCommand(sqlQuery, objConexao);
+            //objConexao.Open();
+            //DataSet ds = new DataSet();
+            //objAdapter.Fill(ds);
+            //GridView1.DataSource = ds;
+            //GridView1.DataBind();
+
+           
+
+
+            if (!IsPostBack)
+            {
+                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo(Server.MapPath("~/arquivos/"));
+                System.IO.FileInfo[] fi = di.GetFiles();
+
+                foreach (System.IO.FileInfo arquivo in fi)
+                    ListBox1.Items.Add(arquivo.Name);
+            }
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+
+            System.IO.FileInfo arquivo = new System.IO.FileInfo(Server.MapPath("~/arquivos/")+ ListBox1.SelectedValue);
+            Response.Clear();
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + arquivo.Name);
+            Response.AddHeader("Content-Length", arquivo.Length.ToString());
+            Response.ContentType = "application/octet-stream";
+            Response.WriteFile(arquivo.FullName);
+            Response.End();
+        }
+
+        
+    }
+}
